b76f5b42edf66f2c94ff45eb8600f00c1fbd6c35
isilbolatbas <isilbltbs@hotmail.com>
2019-07-25 15:09:27 +0300

    Create customDialog with use editText, textView and Spinner

--- app/src/main/java/com/example/vallason/MapFragment.java
+++ app/src/main/java/com/example/vallason/MapFragment.java
@@ -1,6 +1,8 @@
 package com.example.vallason;
 
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,6 +31,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     GoogleMap map;
     FloatingActionButton fab;
     boolean isClicked = false;
+    boolean isDone = false;
+    Context context;
 
 
     public MapFragment() {
@@ -48,26 +53,28 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        context = getContext();
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map1);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         map = googleMap;
 
         fab.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-
                 isClicked = true;
-
+                if (isDone) {
+                    final Dialog dialog = CustomDialog.createDialog(context);
+                    dialog.show();
+                }
             }
 
         });
-
 
         map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
@@ -80,8 +87,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                     map.addMarker(options);
                     map.animateCamera(CameraUpdateFactory.newLatLng(destination));
                     fab.setImageResource(R.drawable.ic_done_black_24dp);
+                    isDone = true;
                 }
-
             }
         });
 
